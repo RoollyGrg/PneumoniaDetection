3fe1ffde9d6506366ddf12f10678b90c96dc9d86
Bibek Gurung <np01cp4a190392@islingtoncollege.edu.np>
2022-03-29 18:08:05 +0545

    information page

--- PneumoniaDetection/Information/Information.swift
+++ PneumoniaDetection/Information/Information.swift
@@ -46,31 +46,6 @@ struct Information: View {
                         .navigationBarTitle("Information")
                     .navigationBarBackButtonHidden(true)
                     
-                    //button for mapView
-                    NavigationLink(destination: DiagTreatment(), label:{
-                    
-                    VStack(alignment:.center, spacing: 10){
-                        
-                        Image(systemName: "pills.fill")
-                            .font(.system(size: 60))
-                            .foregroundColor(.red)
-                            .padding(.bottom, 10)
-                        
-                        Text("Diagnosis & Treatment")
-                            .fontWeight(.medium)
-                            .multilineTextAlignment(.center)
-                            .foregroundColor(.black)
-                        
-                    }
-                    .frame(width: 170, height: 170)
-                    .background(.white)
-                    .cornerRadius(15)
-                    .clipped()
-                    .shadow(color: Color(red: 195/255, green: 195/255, blue: 195/255), radius: 3, x: 0, y: 1)
-                    })
-                        .navigationBarHidden(true)
-                        .navigationBarTitle("Information")
-                        .navigationBarBackButtonHidden(true)
                 }
                 
                 
